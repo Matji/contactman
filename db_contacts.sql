df5a164ac5aa0dfbf61c6c8b01375d9d458c4411
africa <graffiti.nerd@yahoo.com>
2016-07-02 14:58:09 +0200

    last commit

--- db_contacts.sql
+++ db_contacts.sql
@@ -10,7 +10,7 @@ Target Server Type    : MYSQL
 Target Server Version : 50505
 File Encoding         : 65001
 
-Date: 2016-07-01 23:06:04
+Date: 2016-07-02 14:57:30
 */
 
 SET FOREIGN_KEY_CHECKS=0;
@@ -29,8 +29,7 @@ CREATE TABLE `tbl_contacts` (
 -- ----------------------------
 -- Records of tbl_contacts
 -- ----------------------------
-INSERT INTO `tbl_contacts` VALUES ('19', 'Chrsi', 'rock@dgf.com', '01224');
-INSERT INTO `tbl_contacts` VALUES ('20', 'Pharell', 'p@sds', '21321');
-INSERT INTO `tbl_contacts` VALUES ('21', 'Pharey', 'phaere', '54546');
-INSERT INTO `tbl_contacts` VALUES ('22', 'Kidult', 'lkl', '2222');
-INSERT INTO `tbl_contacts` VALUES ('23', 'kidultooo', 'ghfgh', '666');
+INSERT INTO `tbl_contacts` VALUES ('20', 'Christopher Columbus', 'chris@gmail.com', '0152553266');
+INSERT INTO `tbl_contacts` VALUES ('21', 'Shaka Zulu', 'shaka@zulu.com', '0155112266');
+INSERT INTO `tbl_contacts` VALUES ('22', 'Mahatma Ghandi', 'ghand@yahoo.com', '0116845660');
+INSERT INTO `tbl_contacts` VALUES ('23', 'Hitler', 'ww2@hit.com', '0802146793');
